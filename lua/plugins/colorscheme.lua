09116a2e086d25e751b923a3b0a43e6fa33dee93
ananthvk <123456789987654321sr@gmail.com>
2023-10-07 21:15:23 +0530

    commit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,13 @@
-return {
+return {{
     'sainnhe/gruvbox-material',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,
     config = function()
         vim.cmd [[colorscheme gruvbox-material]]
     end
+},
+    {
+        'maxmx03/solarized.nvim',
+        lazy = true,
+    }
 }
---[==[
-return {
-    'maxmx03/solarized.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.o.background = 'dark' -- or 'light'
-
-        vim.cmd.colorscheme 'solarized'
-    end
-}
---]==]
