a63ecbedd8ea33b659e44e18266c5be1efaf25b7
ananthvk <123456789987654321sr@gmail.com>
2023-10-07 19:26:03 +0530

    Added neo tree

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,3 +6,15 @@ return {
         vim.cmd [[colorscheme gruvbox-material]]
     end
 }
+--[==[
+return {
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.o.background = 'dark' -- or 'light'
+
+        vim.cmd.colorscheme 'solarized'
+    end
+}
+--]==]
