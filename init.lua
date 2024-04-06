2d2b486cabb0be04b77255d258ae7d9bdd571014
ananthvk <123456789987654321sr@gmail.com>
2024-04-07 03:24:55 +0530

    Changes

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ if vim.g.vscode then
     -- VSCode extension
     -- Do not install and use plugins here
     print("Using vs nvim")
+
 else
     -- ordinary Neovim
     require("conf.options")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,24 @@
-return {{
-    'sainnhe/gruvbox-material',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000,
-    config = function()
-        vim.o.background = 'dark'
-        vim.cmd [[colorscheme gruvbox-material]]
-    end
-},
+return {
+    {
+        'sainnhe/gruvbox-material',
+        lazy = true, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000,
+        config = function()
+            vim.o.background = 'dark'
+            --vim.cmd [[colorscheme gruvbox-material]]
+        end
+    },
     {
         'maxmx03/solarized.nvim',
         lazy = true,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        config = function()
+            vim.o.background = 'dark'
+            vim.cmd [[colorscheme catppuccin-mocha]]
+        end
     }
 }
